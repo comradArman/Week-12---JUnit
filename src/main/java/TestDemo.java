2efc285cae126851316347eab848c85b3f384af1
unknown <armanhammer12@gmail.com>
2022-12-21 13:35:22 -0700

    first commit

---
+++ src/main/java/TestDemo.java
@@ -0,0 +1,38 @@
+import java.util.Random;
+
+public class TestDemo {
+
+	
+	public int addPositive(int a, int b) {
+		
+		if ((a <= 0) || (b <= 0)){
+			throw new IllegalArgumentException("Both parameters must be positive!");
+			
+		
+		}
+		else {
+		
+			return a + b;
+		}
+			
+	}
+		
+
+
+	
+	 public int randomNumberSquared() {
+	 
+		 int random = getRandomInt();
+			 return random * random;
+			 
+	 }
+	 public int getRandomInt() {
+			 
+			  Random random = new Random();
+			  
+			  return random.nextInt(10) + 1;
+			
+	
+}
+		 
+}
